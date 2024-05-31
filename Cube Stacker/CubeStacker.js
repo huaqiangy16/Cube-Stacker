b1d144a9a7e74735145152d51a18dedcc9b9fea6
Huaqiang Ye <huaqiangy16@g.ucla.edu>
2024-05-30 19:32:01 -0700

    No need movement control
    No need movement control

--- Cube Stacker/CubeStacker.js
+++ Cube Stacker/CubeStacker.js
@@ -204,7 +204,7 @@ class Base_Scene extends Scene {
 
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
-           this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+           //this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(this.camera_matrix);
         }
